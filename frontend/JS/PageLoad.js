d0600584c4fa3f7fefbf19e5550f6de01c4b14bc
Jacob Andersen <jacobvoandersen@gmail.com>
2026-09-01 11:10:35 +0200

    Added the page load function via JQuery

--- frontend/JS/PageLoad.js
+++ frontend/JS/PageLoad.js
@@ -1,1 +1,9 @@
 //Loading of the pages is done here
+
+const pages = ["login.html"]
+
+let selected_page = "./pages/"+pages[0]
+
+$(document).ready(function () {
+  $("#page").append($("<section>").load(selected_page));
+});
