66abfd2f30b7de621720f0fb6a23e2f8df409fb2
Jacob Ørnskov Andersen <jacobvoandersen@gmail.com>
2026-09-07 12:31:24 +0200

    Added the remaining tables to the database

--- backend/DBConn.py
+++ backend/DBConn.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Boolean, String, Integer, Float, ForeignKey, create_engine
+from sqlalchemy import Boolean, String, Integer, DateTime, Float, ForeignKey, create_engine
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship, sessionmaker
 import uuid
 
@@ -36,16 +36,24 @@ class budgetItems(Base):
     itemValue: Mapped[float] = mapped_column(Float, nullable=False)
     itemDescription: Mapped[str] = mapped_column(String(120), nullable=False)
 
-# class userGroups(Base):
-#     __tablename__ = "UserGroups"
+class userGroups(Base):
+    __tablename__ = "UserGroups"
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    group: Mapped[Integer] = mapped_column(ForeignKey("BudgetGroups.groupID"))
+    user: Mapped[Integer] = mapped_column(ForeignKey("Users.userID"))
 
 
-# class groups(Base):
-#     __tablename__ = "BudgetGroups"
+class groups(Base):
+    __tablename__ = "BudgetGroups"
+    groupID: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    groupName: Mapped[str] = mapped_column(String(60), nullable=False)
 
-
-# class sessionLog(Base):
-#     __tablename__ = "SessionLog"
+class sessionLog(Base):
+    __tablename__ = "SessionLog"
+    sessionID: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    user: Mapped[int] = mapped_column(ForeignKey("Users.userID"))
+    created: Mapped[DateTime] = mapped_column(DateTime, nullable=False)
+    ends: Mapped[DateTime] = mapped_column(DateTime, nullable=False)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
