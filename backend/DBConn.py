59ec5a3295e31f5341f17c44a54a455acdf0c69e
Jacob Ørnskov Andersen <jacobvoandersen@gmail.com>
2026-09-07 09:41:28 +0200

    Working on DB and ER diagram

--- backend/DBConn.py
+++ backend/DBConn.py
@@ -2,7 +2,7 @@ from sqlalchemy import Boolean, String, Integer, ForeignKey, create_engine
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship, sessionmaker
 import uuid
 
-engine = create_engine("mysql+pymysql://root:admin@tracky_db:3316/tracky", echo=True)
+engine = create_engine("mysql+pymysql://root:admin@mysql:3306/tracky", echo=True)
 
 class Base(DeclarativeBase):
     pass
@@ -13,6 +13,23 @@ class test(Base):
 
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(String(30))
+
+class users(Base):
+    __tablename__ = "users"
+
+    userID: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    username: Mapped[str] = mapped_column(String(60))
+    password: Mapped[str] = mapped_column(String(60))
+
+class budgets(Base):
+    __tablename__ = "budgets"
+
+    budgetID: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+
+class userBudget(Base):
+    __tablename__ = "userBudget"
+    userID: Mapped[int] = mapped_column(ForeignKey("users.userID"))
+    budgetID: Mapped[int] = mapped_column(ForeignKey("budgets.budgetID"))
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
