326ec82a9bcee53f1df84660df102c91aac99b72
Jacob Andersen <jacobvoandersen@gmail.com>
2026-09-08 17:38:37 +0200

    Added a header on every page, loaded on the same document ready function

--- frontend/JS/PageLoad.js
+++ frontend/JS/PageLoad.js
@@ -6,4 +6,6 @@ let selected_page = "./pages/"+pages[1]
 
 $(document).ready(function () {
   $("#page").append($("<section>").load(selected_page));
+
+  $("#headerInfo").append($("<header>").load("./extras/header.html"));
 });
