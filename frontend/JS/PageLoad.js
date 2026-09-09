1853d2eec6d49a9630c897130ce2b64d3c41d228
Jacob Ørnskov Andersen <jacobvoandersen@gmail.com>
2026-09-09 13:32:02 +0200

    Added temp function on login to create a sessionToken, Sessions are now taken into account when going between pages and redirects the user if no session is found.

--- frontend/JS/PageLoad.js
+++ frontend/JS/PageLoad.js
@@ -5,13 +5,11 @@ const pages = ["login.html", "overview.html"];
 let selected_page = pages[0];
 
 $(document).ready(function () {
-  if (window.localStorage.getItem("lastOpened") == null){
+  if (window.sessionStorage.getItem("lastOpened") == null) {
     GotoPage(selected_page);
+  } else {
+    GotoPage(window.sessionStorage.getItem("lastOpened"));
   }
-  else{
-    GotoPage(window.localStorage.getItem("lastOpened"));
-  }
-  // $("#page").append($("<section>").load(selected_page));
 
   $("#headerInfo").append($("<header>").load("./extras/header.html"));
 });
@@ -21,7 +19,11 @@ function GotoPage(page) {
   if (document.getElementById("page").childNodes.length > 0) {
     document.getElementById("page").firstChild.remove();
   }
-  $("#page").append($("<section>").load("./pages/" + page));
-  window.localStorage.setItem("lastOpened", page)
+  if (window.sessionStorage.getItem("sessionToken") == null){
+    $("#page").append($("<section>").load("./pages/" + pages[0]));
+  }
+  else{
+    $("#page").append($("<section>").load("./pages/" + page));
+  }
+  window.sessionStorage.setItem("lastOpened", page);
 }
-
