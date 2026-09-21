c37303afafe3e4174ff73b49c463c1dd0d04c22e
Jacob Ørnskov Andersen <jacobvoandersen@gmail.com>
2026-09-21 10:15:39 +0200

    Added the start for creating items

--- frontend/JS/connection.js
+++ frontend/JS/connection.js
@@ -53,3 +53,33 @@ async function create_user(username = "", password = "") {
     throw error;
   }
 }
+
+// Items
+async function create_item(itemName = "", itemDesc= "", itemPrice = 0.0) {
+  try {
+    const response = await fetch(`${dev_connection}createItem`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        token: window.sessionStorage.getItem("sessionToken"),
+        itemName: itemName,
+        itemDesc: itemDesc,
+        itemPrice: itemPrice,
+      }),
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data = await response.text();
+    console.log(data, response.status);
+
+    return data;
+  } catch (error) {
+    console.error("Error creating item:", error);
+    throw error;
+  }
+}
