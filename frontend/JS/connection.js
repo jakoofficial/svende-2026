de842abc45154afd01cdf4c5f79941995c48a97a
Jacob Ørnskov Andersen <jacobvoandersen@gmail.com>
2026-09-21 09:45:20 +0200

    Added functionallity to create and sign user in on frontend part

---
+++ frontend/JS/connection.js
@@ -0,0 +1,55 @@
+const deploy_connection = "api.tracky.boldbyte.dev/";
+const dev_connection = "http://localhost:8010/";
+
+async function user_signin(username, password) {
+  try {
+    const response = await fetch(`${dev_connection}login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username: username,
+        password: password,
+      }),
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data = await response.json();
+
+    return data;
+  } catch (error) {
+    console.error("Error signing in", error);
+    throw error;
+  }
+}
+
+async function create_user(username = "", password = "") {
+  try {
+    const response = await fetch(`${dev_connection}createUser`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username: username,
+        password: password,
+      }),
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data = await response.text();
+    console.log(data, response.status);
+
+    return data;
+  } catch (error) {
+    console.error("Error creating user:", error);
+    throw error;
+  }
+}
