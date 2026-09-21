4a38c37d51a8a4fabfc6a3afc4f32b475437f630
Jacob Ørnskov Andersen <jacobvoandersen@gmail.com>
2026-09-21 11:32:09 +0200

    Added https

--- frontend/JS/connection.js
+++ frontend/JS/connection.js
@@ -1,5 +1,5 @@
 const deploy_connection = "api.tracky.boldbyte.dev/";
-const dev_connection = "api.tracky.boldbyte.dev/";
+const dev_connection = "https://api.tracky.boldbyte.dev/";
 
 async function user_signin(username, password) {
   try {
